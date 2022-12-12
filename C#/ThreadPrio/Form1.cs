bd2f373998433091956926217167dfa4726f99bd
NarixenoAdaxius <arionedauis@gmail.com>
2022-12-12 11:47:02 +0800

    updates

---
+++ C#/ThreadPrio/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ThreadPrio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread ThreadA = new Thread(MyThreadClass.Thread1);
+            ThreadA.Name = "Thread A Process";
+            Thread ThreadB = new Thread(MyThreadClass.Thread2);
+            ThreadB.Name = "Thread B Process";
+            Thread ThreadC = new Thread(MyThreadClass.Thread1);
+            ThreadC.Name = "Thread C Process";
+            Thread ThreadD = new Thread(MyThreadClass.Thread2);
+            ThreadD.Name = "Thread D Process";
+
+            Console.WriteLine("--Thread Starts--");
+
+            ThreadA.Start();
+            ThreadB.Start();
+            ThreadC.Start();
+            ThreadD.Start();
+
+            ThreadA.Join();
+            ThreadB.Join();
+            ThreadC.Join();
+            ThreadD.Join();
+
+            Console.WriteLine("--End of Thread--");
+            label1.Text = "--End of Thread-- ";
+        }
+    }
+}
---
+++ C#/ThreadPrio/MyThreadClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPrio
+{
+	public class MyThreadClass
+	{
+		public static void Thread1()
+		{
+			for (int i = 0; i < 2; i++){
+				Thread thread = Thread.CurrentThread;
+				Console.WriteLine("Name of Thread: " + thread.Name + " = " + i);
+				Thread.Sleep(500);
+			} 
+		}
+		public static void Thread2()
+		{
+			for (int i = 0; i <= 5; i++){
+				Thread thread = Thread.CurrentThread;
+				Console.WriteLine("Name of Thread: " + thread.Name + " = " + i);
+				Thread.Sleep(500);
+			}
+		}
+	}
+}
